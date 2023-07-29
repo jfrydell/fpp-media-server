ee9e662b7c5ab9e082aa4e2eee54d28ab0de7145
Fletch Rydell <fletch.rydell@duke.edu>
2023-07-29 13:04:54 -0700

    api based versino

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use std::sync::{Arc, RwLock};
+use std::{
+    collections::VecDeque,
+    sync::{Arc, RwLock},
+    time::SystemTime,
+};
 
 use axum::{
     extract::{
         ws::{Message, WebSocket},
         State, WebSocketUpgrade,
     },
-    response::Response,
+    response::{IntoResponse, Response},
     routing::{get, post},
     Json, Router,
 };
@@ -21,6 +25,7 @@ async fn main() {
     let state = Arc::new(RwLock::new(CurrentState::default()));
     let router = Router::new()
         .route("/", post(handle_sync_event))
+        .route("/api/start_time", get(get_start_time))
         .route("/ws", get(upgrade_ws))
         .with_state((state, update_sender))
         .fallback_service(ServeDir::new("./content"));
@@ -30,6 +35,31 @@ async fn main() {
         .unwrap();
 }
 
+async fn get_start_time(State(state): State<AppState>) -> Result<impl IntoResponse, ()> {
+    let state = state.0.read().unwrap();
+    #[derive(Serialize)]
+    struct Response {
+        filename: Option<String>,
+        start_time: f64,
+    }
+    let avg_start = state
+        .start_times
+        .iter()
+        .copied()
+        .reduce(|a, b| a + b)
+        .map(|s| s / state.start_times.len() as f64)
+        .unwrap_or(
+            SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs_f64(),
+        );
+    Ok(Json(Response {
+        filename: state.filename.clone(),
+        start_time: avg_start,
+    }))
+}
+
 async fn handle_sync_event(State(app_state): State<AppState>, Json(event): Json<Event>) {
     let mut state = app_state.0.write().unwrap();
     match event {
@@ -37,6 +67,7 @@ async fn handle_sync_event(State(app_state): State<AppState>, Json(event): Json<
             if id >= state.id || state.id - id > 100 {
                 state.id = id;
                 state.filename = Some(filename);
+                state.start_times.clear();
             }
         }
         Event::MediaStop { id } => {
@@ -50,9 +81,19 @@ async fn handle_sync_event(State(app_state): State<AppState>, Json(event): Json<
             time,
             latencies,
         } => {
-            if id == state.id {
+            if id >= state.id {
                 let avg_latency = latencies.iter().sum::<f64>() / latencies.len() as f64;
                 state.time = time + avg_latency;
+                // Calculate start time of sequence
+                let current_time = SystemTime::now()
+                    .duration_since(SystemTime::UNIX_EPOCH)
+                    .unwrap();
+                let start_time = current_time.as_secs_f64() - state.time;
+                // Add to queue
+                state.start_times.push_back(start_time);
+                if state.start_times.len() > 20 {
+                    state.start_times.pop_front();
+                }
             } else {
                 println!("Sync event with wrong id: {} (should be {})", id, state.id);
             }
@@ -100,4 +141,6 @@ struct CurrentState {
     #[serde(skip)]
     id: i32,
     time: f64,
+    #[serde(skip)]
+    start_times: VecDeque<f64>,
 }
